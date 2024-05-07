9d66942e3b89cbc0aeb13a364c66160f6a29db11
Nuytaa <bestsisterann@gmail.com>
2024-05-07 15:33:19 +0300

    добавлен gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './',
-})
+ // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   base: './',
+// })
+
+const {defineConfig} = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath:'/weather/',
+  transpileDependencies: true
+});
